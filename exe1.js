e8e57bf5ba37044f0711d601ca614ba7fe4636bc
hassan <hassan.zandipoor@gmail.com>
2022-12-10 00:20:31 +0330

    update4

--- exe1.js
+++ exe1.js
@@ -1,9 +1,8 @@
-// function count(num) {
-//     return num.reduce((depth, index) => {
-//       return Math.max(depth, 1 + count(index.num)); 
-//     }, 0);
-//   }
+function count(num) {
+    return num.reduce((depth, index) => {
+      return Math.max(depth, 1 + count(index.num)); 
+    }, 0);
+  }
    
-//   console.log(count([10, [25, 13], [14, [55]], 2]))
+  console.log(count([10, [25, 13], [14, [55]], 2]))
 
-function 
